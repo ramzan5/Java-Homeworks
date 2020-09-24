a22a4f23f18890a45502450e48618952f8258b7b
Muhammad Ramzan <ramzanmechanical@gmail.com>
2020-09-24 10:24:10 +0200

    modified

--- lab02HW.java
+++ lab02HW.java
@@ -1,8 +1,18 @@
-public class lab02HW{
-	public static void main(String[] args){
-		if (args.length < 2){
+public class lab02HW{	
+	public static void main(String[] a){
+		long maxiValue = Long.parseLong(a[0]);
+		if (a.length < 2){
 			System.err.println("Please provide me with at least two parameters: TRY AGAIN!");
 			System.exit(1);
 		}
+		for(int i=0;i<a.length;i++)
+		{
+			if(Long.parseLong(a[i]) >= maxiValue)
+			{
+				maxiValue = Long.parseLong(a[i]);
+			
+			}
+		}
+		System.out.println("Max Value is " + maxiValue);
 	}
 }
